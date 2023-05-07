9c9329d69f64569fdcad3d9b0da38026da861234
bcrangaci <117463510+bcrangaci@users.noreply.github.com>
2023-05-07 17:27:38 +0300

    Update index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyDir() {
+  const sourceDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files-copy');
+
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir);
+  }
+
+  fs.readdirSync(sourceDir).forEach((file) => {
+    const sourceFile = path.join(sourceDir, file);
+    const destFile = path.join(destDir, file);
+    const fileStats = fs.statSync(sourceFile);
+
+    if (fileStats.isFile()) {
+      const readStream = fs.createReadStream(sourceFile);
+      const writeStream = fs.createWriteStream(destFile);
+
+      readStream.pipe(writeStream);
+    }
+  });
+}
+
+copyDir();
 
